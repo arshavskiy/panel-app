35e16774b8999a19151f74abd76924c82997fe9d
Pavel <arshavsky.pasha@gmail.com>
2018-01-22 22:56:03 +0200

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GraphComponent } from './components/graph/graph.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { ActivityComponent } from './components/activity/activity.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphComponent,
+    MainNavComponent,
+    SalesComponent,
+    TasksComponent,
+    MessagesComponent,
+    ActivityComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
