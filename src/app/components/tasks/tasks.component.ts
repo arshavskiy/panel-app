becfb5b7befdcda75d4ee6d8d34f4630299b1b41
Pavel Arshavsky <arshavsky.pasha@gmail.com>
2018-01-24 18:28:23 +0200

    date

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tasks',
+  selector: '[app-tasks]',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+  date: any;
+  title = "rrrr";
 
   constructor() { }
 
   ngOnInit() {
+
+    this.date = this.dateBuilder(2012,1,0);
+    console.log(this.date);
+
   }
+
+
+  dateBuilder(y:number,d:number,m:number){
+    var newDate = new Date(y, d, m);
+    return newDate
+  }
+
+
+
 
 }
