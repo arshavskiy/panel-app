7c95365b04577b2c2ba267588292a568882cc832
Pavel <arshavsky.pasha@gmail.com>
2018-01-28 22:30:12 +0200

    graph ng2-chart added

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -7,11 +7,9 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GraphComponent implements OnInit {
 
-
   public lineChartData: Array<any> = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    // {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    // {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    // {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
   ];
   public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: any = {
@@ -33,14 +31,6 @@ export class GraphComponent implements OnInit {
     //   pointBorderColor: '#fff',
     //   pointHoverBackgroundColor: '#fff',
     //   pointHoverBorderColor: 'rgba(77,83,96,1)'
-    // },
-    // { // grey
-    //   backgroundColor: 'rgba(148,159,177,0.2)',
-    //   borderColor: 'rgba(148,159,177,1)',
-    //   pointBackgroundColor: 'rgba(148,159,177,1)',
-    //   pointBorderColor: '#fff',
-    //   pointHoverBackgroundColor: '#fff',
-    //   pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     // }
   ];
   public lineChartLegend: boolean = true;
