a65f12754d326e47cd123dc95b4e1f6369ec20a1
Pavel <arshavsky.pasha@gmail.com>
2018-01-28 23:24:17 +0200

    second graph added

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -7,6 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class GraphComponent implements OnInit {
 
+  title: string = 'Report';
   public lineChartData: Array<any> = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
     // {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
--- src/app/components/sales/sales.component.ts
+++ src/app/components/sales/sales.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesComponent implements OnInit {
 
+  // Doughnut
+  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartType:string = 'doughnut';
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+
   constructor() { }
 
   ngOnInit() {
