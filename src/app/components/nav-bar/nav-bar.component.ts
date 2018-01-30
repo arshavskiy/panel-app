08e2f9e9bef7c682c2badbfd43a5080b2e1a5157
Pavel <arshavsky.pasha@gmail.com>
2018-01-31 00:21:50 +0200

    active class

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements  OnInit{
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    document.body.classList.add('active');
   }
 
   toggle() {
-    console.log(event);
+    if (document.body.classList.length > 0) {
+      document.body.classList.remove('active');
+
+    } else {
+      document.body.classList.add('active');
+    }
   }
 }
