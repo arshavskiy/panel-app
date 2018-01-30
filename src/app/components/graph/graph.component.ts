8649c51e095fc030bf34966018ddb0ae5f723f28
Pavel <arshavsky.pasha@gmail.com>
2018-01-30 22:54:13 +0200

    nav-bar des

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -9,7 +9,7 @@ export class GraphComponent implements OnInit {
 
   title: string = 'Report';
   public lineChartData: Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Sales'},
     // {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
   ];
   public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
