b1cea7a99a6765a6016612c257e688eb96820317
Pavel <arshavsky.pasha@gmail.com>
2018-02-02 19:34:56 +0200

    nav style on open added

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -4,14 +4,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class ServerService {
 
+  apiRoot: string = 'https://jsonplaceholder.typicode.com';
+
   constructor(private http: HttpClient ) { }
 
   getTasks() {
-    // return this.tasks;
-    return this.http.get('https://jsonplaceholder.typicode.com/todos');
+    let url = `${this.apiRoot}/todos`;
+    return this.http.get(url);
   }
   getMsg(){
-    return this.http.get('https://jsonplaceholder.typicode.com/users');
+    let url = `${this.apiRoot}/users`;
+    return this.http.get(url);
 
   }
 }
