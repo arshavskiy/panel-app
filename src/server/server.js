75ada64d203bbe396e510f27421fdc5f9acb6548
Pavel <arshavsky.pasha@gmail.com>
2018-03-15 20:33:44 +0200

    working scap with get not full method

--- src/server/server.js
+++ src/server/server.js
@@ -16,102 +16,77 @@ const config = {
 };
 let T = new Twit(config);
 
-var client = new Twitter({
-  consumer_key: 'ZNUDph6kxp4gXNztuwlxM9hjt',
-  consumer_secret: 'TxZvvq23mQyIPeKXoZjUbMIUwqyXSfhOghmF4zbelrAMxjwuGS',
-  access_token: '2574428706-5bwdKynS3zxPfz2aOZ4GuO6edDHvmR48inRSYtX',
-  access_token_secret: 'VkVsXyTnbC43DKQjmMBG5FDkr3dcWhC3DEh4qGtgVhEg2'
-});
-
 
 function getMePosts(q, count) {
 
-  let dataPushed = [];
   let params = {
     q: q,
-    t: 'since:2017-01-01',
+    t: 'since:2018-01-01',
     count: count
   }
 
-  T.get('search/tweets', params, (err, data, response) => {
-    let tweets = data.statuses;
-    for (let i = 0, l = tweets.length; i < l; i++) {
-      // console.log('we found ' + i + ' result' + ' ' + tweets[i].text);
-      dataPushed[i] = tweets[i].text;
-      fs.appendFile('./ufo.txt', tweets[i].user.name + ' \r: ' + tweets[i].text + '-- \r' + tweets[i].user.created_at + '\r\n', 'utf8', (err) => {
-        if (err) throw err;
-      });
-    }
-    console.log(dataPushed[0] + '...');
-  });
-  return dataPushed;
-}
+  T.get('search/tweets', params, gotData);
 
+  function gotData(err, data, res) {
+    let tweets = data.statuses;
+    let dataPushedArray = new Array();
+
+    for (let i = 0, l = tweets.length; i < l; i++) {
+
+      dataPushedArray[i] = {
+        name: tweets[i].user.name,
+        text: tweets[i].text,
+        url: tweets[i].user.profile_background_image_url,
+        date: tweets[i].user.created_at
+      };
+    }
+    // fs.appendFile('./ufo2.txt', tweets[i].user.name + ' \r: ' + tweets[i].text + '-- \r' + tweets[i].user.created_at + '\r\n', 'utf8', (err) => {
+    //   if (err) throw err;
+    // });
+    // fs.appendFile('./ufo.json', JSON.stringify({"name":tweets[i].user.name, "text":tweets[i].text, "date":tweets[i].user.created_at}, null, 2), (err) => {
+    //   if (err) throw err;
+    // });
+
+    fs.appendFile('./data/Scrapper-' + tweets[0].user.name + '_.json', JSON.stringify(dataPushedArray, null, 2), (err) => {
+      if (err) throw err;
+      console.log('file done');
+    });
+
+    if (err) {
+      console.log(err)
+    }
+    return dataPushedArray;
+  }
+}
 
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
 
-let port = process.env.PORT || 8080;
+  let port = process.env.PORT || 8080;
 
 // ROUTES FOR OUR API
-var router = express.Router();
+  var router = express.Router();
 
-router.get('/', function (req, res) {
-  res.json({message: 'hooray! welcome to our api!'});
-});
-
-app.use('/api', router);
-
-
-router.route('/twits/:q,:count')
-  .get(function (req, res) {
-    let q = req.body.name;
-    getMePosts(req.params.q, req.params.count)
-    res.send('got it?');
+  router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to our api!'});
   });
 
-let params = {
-  q: 'ufo',
-  t: 'since:2018-01-01',
-  count: 10
-}
+
+  app.use('/api', router);
 
 
-T.get('search/tweets', params, gotData);
+  router.route('/twits/:q,:count')
+    .get(function (req, res) {
+      let q = req.body.name;
+      getMePosts(req.params.q, req.params.count);
+      res.send('got it?');
+    });
 
-function gotData(err, data, res) {
-  let tweets = data.statuses;
-  let dataPushedArray = new Array();
 
-  for (let i = 0, l = tweets.length; i < l; i++) {
-
-    dataPushedArray[i] = {
-      name: tweets[i].user.name,
-      text: tweets[i].text,
-      url: tweets[i].user.profile_background_image_url,
-      date: tweets[i].user.created_at
-    };
-  }
-  // fs.appendFile('./ufo2.txt', tweets[i].user.name + ' \r: ' + tweets[i].text + '-- \r' + tweets[i].user.created_at + '\r\n', 'utf8', (err) => {
-  //   if (err) throw err;
-  // });
-  // fs.appendFile('./ufo.json', JSON.stringify({"name":tweets[i].user.name, "text":tweets[i].text, "date":tweets[i].user.created_at}, null, 2), (err) => {
-  //   if (err) throw err;
-  // });
-
-  fs.appendFile('./ufo_' + tweets[0].user.name + '_.json', JSON.stringify(dataPushedArray, null, 2), (err) => {
-    if (err) throw err;
-    console.log('file done');
-  });
-
-  if (err) {
-    console.log(err)
-  }
-  return dataPushedArray;
-}
 
 // START THE SERVER
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Magic happens on port ' + port)
+
 
